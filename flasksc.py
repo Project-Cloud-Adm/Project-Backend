efe96c7ed4cb38b9b4dcec9c867df802a8c1f3c5
BartoszBury1 <91828936+BartoszBury1@users.noreply.github.com>
2023-03-04 13:14:10 +0100

    Flask endpoints

--- flasksc.py
+++ flasksc.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/Hello", methods = ["GET", "POST"])
+def hello_name():
+    if request.method == "POST":
+        name = request.form ["name"]
+        return "Hello {}".format(name)
+    else:
+        return '''
+        <form method = "post">
+            <label for = "name">Enter your name: </label>
+            <input type= "text" id = "name" name = "name">
+            <input type = "submit" value = "Submit">
+        </form>
+    '''
+
 if __name__ == "__main__":
     app.run()
